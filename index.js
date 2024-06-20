8e9afa5657707cd8fd728609b1539601b359c24a
pkravella <137841648+pkravella@users.noreply.github.com>
2024-06-20 12:30:44 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('.container');
+const jokeText = document.querySelector('.container p');
+document.addEventListener('DOMContentLoaded', getJoke);
+
+button.addEventListener("click", getJoke);
+
+function getJoke(){
+  fetch('https://icanhazdadjoke.com/', {
+    headers:{
+      'Accept': 'application/json'
+    }
+  }).then(data => data.json())
+    .then(obj => jokeText.innerHTML = obj.joke)
+}
